aa2056434ccd193f15393b48d167eba10d43bc52
Blake Herrera <blake@blakeherrera.com>
2022-06-05 13:20:07 -0500

    added new game settings screen

---
+++ main/scripts/settings.lua
@@ -0,0 +1,10 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local M = {}
+
+M.time = 60
+M.class = "601D"
+
+return M

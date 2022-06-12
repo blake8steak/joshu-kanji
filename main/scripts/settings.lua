f0581f0c0c822dd24f36e1cd699fb33b7c03cb71
Blake Herrera <blake@blakeherrera.com>
2022-06-12 01:48:24 -0400

    added puzzles and rand kanji gen based on class

--- main/scripts/settings.lua
+++ main/scripts/settings.lua
@@ -1,10 +1,6 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
 local M = {}
 
 M.time = 60
-M.class = "601D"
+M.class = "610D"
 
 return M
